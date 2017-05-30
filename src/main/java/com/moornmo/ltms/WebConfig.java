9470763a679b00e97aadf0580de09224df656f63
jinyoung jang <jyjang@uengine.org>
2017-05-30 11:35:49 +0900

    configuration for Metadata framework & uEngine resource manager

--- src/main/java/com/moornmo/ltms/WebConfig.java
+++ src/main/java/com/moornmo/ltms/WebConfig.java
@@ -1,7 +1,10 @@
 package com.moornmo.ltms;
 
-import hello.PersonRepository;
+import org.metaworks.multitenancy.ClassManager;
+import org.metaworks.multitenancy.CouchbaseMetadataService;
+import org.metaworks.multitenancy.MetadataService;
 import org.metaworks.rest.MetaworksRestService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -9,12 +12,16 @@ import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.uengine.modeling.resource.CachedResourceManager;
+import org.uengine.modeling.resource.ResourceManager;
+import org.uengine.modeling.resource.Storage;
+import org.uengine.persistence.couchbase.CouchbaseStorage;
 
 import java.util.List;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackageClasses = {MetaworksRestService.class, ProductRepository.class})
+@ComponentScan(basePackageClasses = {MetaworksRestService.class, ProductRepository.class, ClassManager.class, MetadataService.class})
 public class WebConfig extends WebMvcConfigurerAdapter {
 //    @Override
 //    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
@@ -33,5 +40,37 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 //        registry.addMapping("/people").allowedOrigins("*");
 //        registry.addMapping("/**").allowedOrigins("http://localhost:8081");
 //        registry.addMapping("/people").allowedOrigins("http://localhost:8081");
+    }
+
+    @Bean
+    public ResourceManager resourceManager(){
+        ResourceManager resourceManager = new CachedResourceManager();
+        resourceManager.setStorage(storage());
+        return resourceManager;
+    }
+
+    @Bean
+    /**
+     *
+     * <property name="basePath" value="/"/>
+     <property name="bucketName" value="default"/>
+     <property name="serverIp" value="localhost"/>
+
+     */
+    public Storage storage() {
+        CouchbaseStorage storage = new CouchbaseStorage();
+        storage.setBucketName("default");
+        storage.setServerIp("localhost");
+
+        return storage;
+    }
+
+    @Bean
+    public MetadataService metadataService() {
+        CouchbaseMetadataService metadataService = new CouchbaseMetadataService();
+        metadataService.setCouchbaseServerIp("localhost");
+        metadataService.setBucketName("default");
+
+        return metadataService;
     }
 }
